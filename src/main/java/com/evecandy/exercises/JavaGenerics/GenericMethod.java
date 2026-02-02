826b3dfd15e67c6f96c6484df8f0483339b707b8
candy <evecandymutunga@gmail.com>
2026-02-02 13:18:53 +0300

    generics

---
+++ src/main/java/com/evecandy/exercises/JavaGenerics/GenericMethod.java
@@ -0,0 +1,20 @@
+package com.evecandy.exercises.JavaGenerics;
+
+public class GenericMethod {
+    public static <T> void printArray(T[] inputArray) {
+        for (T element : inputArray) {
+            System.out.println(element);
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = { "Hello", "World" };
+
+        printArray(intArray);
+        printArray(stringArray);
+
+    }
+}
